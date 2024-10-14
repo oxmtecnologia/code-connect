343effe2512ba01d8558b04906bd10ac0d74b248
oxmtecnologia <oxmtecnologia@gmail.com>
2024-10-14 08:44:47 -0300

    first commit

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import styles from './avatar.module.css'
+
+export const Avatar = ({ author }) => {
+    const imgSrc = author.avatar ?? author.image
+
+    return (
+        <div className={styles.container}>
+            {imgSrc && <Image
+                src={imgSrc}
+                width={32}
+                height={32}
+                alt={`Avatar do(a) ${author.name}`}
+            />}
+        </div>
+    )
+}
---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+import styles from './comment.module.css'
+import { Avatar } from "../Avatar"
+
+export const Comment = ({ comment }) => {
+
+
+    return (<div className={styles.comment}>
+        <Avatar author={comment.author} />
+        <strong>@{comment.author.name}</strong>
+        <p>{comment.text}</p>
+    </div>)
+}
